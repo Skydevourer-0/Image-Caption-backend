b1be59a43854c06774048a727781c834a30f79cc
skydevourer <skydevourer@foxmail.com>
2024-02-15 10:26:58 +0800

    初始化Spring框架

---
+++ src/main/java/com/example/imagecaptionbackend/ImageCaptionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagecaptionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCaptionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageCaptionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imagecaptionbackend/ImageCaptionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagecaptionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCaptionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
