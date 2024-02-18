df07e6cc11aca319d27725e369dfba74e6da0c44
skydevourer <skydevourer@foxmail.com>
2024-02-18 23:20:23 +0800

    删除不必要的重写方法

--- src/main/java/com/example/imagecaptionbackend/repository/ImageRepository.java
+++ src/main/java/com/example/imagecaptionbackend/repository/ImageRepository.java
@@ -4,9 +4,7 @@ import com.example.imagecaptionbackend.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
-    List<Image> findByUserId(Long userId);
 }
